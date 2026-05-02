86f4ced9c18ce1e6118d718ee70128b769fc6381
Kaguya-aa <shinomiya.kaguya.daisuki@gmail.com>
2026-05-02 18:36:04 +0800

    Create p2.cpp

---
+++ Simulate/2025.12.09 CPE/p2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+bool cmp(int a, int b)
+{
+	if (a % m != b % m) return (a % m) < (b % m);
+	bool is_odd_a = abs(a % 2) == 1;
+	bool is_odd_b = abs(b % 2) == 1;
+	if (is_odd_a != is_odd_b) return is_odd_a;
+	if (is_odd_a and is_odd_b) return a > b;
+	return b > a;
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	while (cin >> n >> m)
+	{
+		cout << n << " " << m << '\n';
+		if (n == 0 and m == 0) break;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) cin >> a[i];
+		sort(a.begin(), a.end(), cmp);
+		for (int i = 0; i < n; i++) cout << a[i] << '\n';
+	}
+}
